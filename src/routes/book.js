01edd98be777543724ef9ff2fdd52839583486a7
MasKni <masakoni13@gmail.com>
2022-12-10 20:48:57 +0200

    added middleware

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const request = require('request')
-
+const log = require('../middleware/log')
 const router = express.Router()
 // check book 
 const checkBooks = (id)=>{
  
     return new Promise((resolve, reject)=>{
-        const url = 'http://192.168.56.1:5000/books/info/'+id
+        const url = 'http://192.168.56.101:3000/books/info/'+id
         request({url, json:true}, (error, res, body)=>{
             if(error){
                 console.log('check')
@@ -14,7 +14,7 @@ const checkBooks = (id)=>{
 
             } else if(res.statusCode ==404){
                 console.log('check2')
-                return reject(body)
+                return reject("no book like this ")
             }  
             const count = body.numberinstock
             if(count<=0){
@@ -44,9 +44,9 @@ const buyBook = (id,count)=>{
     }) 
 }
 //purchase a book
-router.get('/books/purchase/:id', async(req, res)=>{
+
+router.get('/books/purchase/:id', log, async(req, res)=>{
     try{
-        
         const id = req.params.id
         const checked = await checkBooks(id)
         console.log(checked)
@@ -57,4 +57,5 @@ router.get('/books/purchase/:id', async(req, res)=>{
       res.status(404).send(error)
     }
 })
+
 module.exports = router
